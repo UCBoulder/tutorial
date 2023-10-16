f11a9ad2de56ffdd86268040bbba7bf92b0e5083
protitude <miles.france@gmail.com>
2023-10-16 16:49:44 +0000

    fix js step copy button

--- js/tutorial-node-form.js
+++ js/tutorial-node-form.js
@@ -1,17 +1,17 @@
 (function ($) {
   $(document).ready(function () {
     function updateStepField() {
-      $( ".field--name-field-tut-body-image table tbody tr" ).each(function ( index, element ) {
-        var id = $(this).find(".image-widget input[type=hidden]").attr('value');
+      $( ".file-widget-multiple__table-wrapper table tbody tr" ).each(function ( index, element ) {
+        var id = $(this).find("span.file--image").attr('data-drupal-selector');
         var hiddenInputId = $(this).find(".image-widget input[type=hidden]").attr('data-drupal-selector');
         // Theme switched to using the wieght of the draggable item over the fid
         // passing this along to correctly select the title input.
         var stepWeight = thenum = hiddenInputId.match(/\d+/)[0];
         id = id.match(/\d+/g).map(Number);
-        $(this).find('.tabledrag-hide').prev().prev().prev().prev().find('.form-item-field-tut-body-image-current-' + id + '-meta-title').find('label').text("Step Text");
-        $(this).find('.tabledrag-hide').prev().prev().prev().prev().find('.form-item-field-tut-body-image-current-' + id + '-meta-title').find('.description').text("This text will be added to your clipboard for the text in the step.");
+        $(this).find('.tabledrag-hide').prev().prev().prev().prev().find('.form-item-field-tut-body-image-current-' + id[1] + '-meta-title').find('label').text("Step Text");
+        $(this).find('.tabledrag-hide').prev().prev().prev().prev().find('.form-item-field-tut-body-image-current-' + id[1] + '-meta-title').find('.description').text("This text will be added to your clipboard for the text in the step.");
         if ( !$(this).find('.tabledrag-hide').prev().find('button.copy-tutorial-fid').length ) {
-          $(this).find('.tabledrag-hide').prev().append('<button type="button" class="copy-tutorial-fid" onclick="copyFid(' + id + ", " + stepWeight + ')">Copy Step</button><br/><br/><strong>FID:</strong> ' + id);
+          $(this).find('.tabledrag-hide').prev().append('<button type="button" class="copy-tutorial-fid" onclick="copyFid(' + id[1] + ", " + stepWeight + ')">Copy Step</button><br/><br/><strong>FID:</strong> ' + id[1]);
         }
       });
     }
